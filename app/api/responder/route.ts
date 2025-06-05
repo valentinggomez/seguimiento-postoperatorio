d330671541427e75b442fde73012d3c6b5ec9467
Valentín Gómez <ids41gomezvalen@gmail.com>
2025-06-05 02:29:08 -0300

    Usar GOOGLE_CREDENTIALS como variable unificada

--- app/api/responder/route.ts
+++ app/api/responder/route.ts
@@ -8,18 +8,7 @@ const SHEET_ID = '1cSWp5jBz2nLSMbWbbontNNODSLGclN8TOVxh-TmV3aY';
 const SHEET_NAME = 'Seguimiento';
 
 // @ts-ignore
-const credentials = {
-  type: "service_account",
-  project_id: process.env.GCP_PROJECT_ID,
-  private_key_id: process.env.GCP_PRIVATE_KEY_ID,
-  private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  client_email: process.env.GCP_CLIENT_EMAIL,
-  client_id: process.env.GCP_CLIENT_ID,
-  auth_uri: process.env.GCP_AUTH_URI,
-  token_uri: process.env.GCP_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.GCP_AUTH_PROVIDER,
-  client_x509_cert_url: process.env.GCP_CLIENT_CERT_URL,
-};
+const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS!);
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
